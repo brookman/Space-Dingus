e2f3297da74a8c9191a7cb3bd9a0d8f8de717cac
brookman <rc-p90@bluemail.ch>
2013-08-11 14:47:02 +0200

    Removed UUID, moved BitSet

--- core/src/main/java/com/artemis/Entity.java
+++ core/src/main/java/com/artemis/Entity.java
@@ -1,7 +1,6 @@
 package com.artemis;
 
 import java.util.BitSet;
-import java.util.UUID;
 
 import com.artemis.utils.Bag;
 
@@ -13,7 +12,6 @@ import com.artemis.utils.Bag;
  * 
  */
 public final class Entity {
-   private UUID uuid;
 
    private int id;
    private BitSet componentBits;
@@ -35,8 +33,8 @@ public final class Entity {
    }
 
    /**
-    * The internal id for this entity within the framework. No other entity
-    * will have the same ID, but ID's are however reused so another entity may
+    * The internal id for this entity within the framework. No other entity will
+    * have the same ID, but ID's are however reused so another entity may
     * acquire this ID if the previous entity was deleted.
     * 
     * @return id of the entity.
@@ -46,7 +44,8 @@ public final class Entity {
    }
 
    /**
-    * Returns a BitSet instance containing bits of the components the entity possesses.
+    * Returns a BitSet instance containing bits of the components the entity
+    * possesses.
     * 
     * @return
     */
@@ -55,7 +54,8 @@ public final class Entity {
    }
 
    /**
-    * Returns a BitSet instance containing bits of the components the entity possesses.
+    * Returns a BitSet instance containing bits of the components the entity
+    * possesses.
     * 
     * @return
     */
@@ -64,13 +64,11 @@ public final class Entity {
    }
 
    /**
-    * Make entity ready for re-use.
-    * Will generate a new uuid for the entity.
+    * Make entity ready for re-use. Will generate a new uuid for the entity.
     */
    protected void reset() {
       systemBits.clear();
       componentBits.clear();
-      uuid = UUID.randomUUID();
    }
 
    @Override
@@ -81,7 +79,8 @@ public final class Entity {
    /**
     * Add a component to this entity.
     * 
-    * @param component to add to this entity
+    * @param component
+    *           to add to this entity
     * 
     * @return this entity for chaining.
     */
@@ -91,11 +90,13 @@ public final class Entity {
    }
 
    /**
-    * Faster adding of components into the entity. Not neccessery to use this, but
-    * in some cases you might need the extra performance.
+    * Faster adding of components into the entity. Not neccessery to use this,
+    * but in some cases you might need the extra performance.
     * 
-    * @param component the component to add
-    * @param type of the component
+    * @param component
+    *           the component to add
+    * @param type
+    *           of the component
     * 
     * @return this entity for chaining.
     */
@@ -107,7 +108,8 @@ public final class Entity {
    /**
     * Removes the component from this entity.
     * 
-    * @param component to remove from this entity.
+    * @param component
+    *           to remove from this entity.
     * 
     * @return this entity for chaining.
     */
@@ -119,7 +121,8 @@ public final class Entity {
    /**
     * Faster removal of components from a entity.
     * 
-    * @param component to remove from this entity.
+    * @param component
+    *           to remove from this entity.
     * 
     * @return this entity for chaining.
     */
@@ -141,8 +144,8 @@ public final class Entity {
    }
 
    /**
-    * Checks if the entity has been added to the world and has not been deleted from it.
-    * If the entity has been disabled this will still return true.
+    * Checks if the entity has been added to the world and has not been deleted
+    * from it. If the entity has been disabled this will still return true.
     * 
     * @return if it's active.
     */
@@ -151,9 +154,9 @@ public final class Entity {
    }
 
    /**
-    * Will check if the entity is enabled in the world.
-    * By default all entities that are added to world are enabled,
-    * this will only return false if an entity has been explicitly disabled.
+    * Will check if the entity is enabled in the world. By default all entities
+    * that are added to world are enabled, this will only return false if an
+    * entity has been explicitly disabled.
     * 
     * @return if it's enabled
     */
@@ -163,13 +166,12 @@ public final class Entity {
 
    /**
     * This is the preferred method to use when retrieving a component from a
-    * entity. It will provide good performance.
-    * But the recommended way to retrieve components from an entity is using
-    * the ComponentMapper.
+    * entity. It will provide good performance. But the recommended way to
+    * retrieve components from an entity is using the ComponentMapper.
     * 
     * @param type
-    *        in order to retrieve the component fast you must provide a
-    *        ComponentType instance for the expected component.
+    *           in order to retrieve the component fast you must provide a
+    *           ComponentType instance for the expected component.
     * @return
     */
    public Component getComponent(ComponentType type) {
@@ -182,21 +184,22 @@ public final class Entity {
     * components.
     * 
     * @param <T>
-    *        the expected return component type.
+    *           the expected return component type.
     * @param type
-    *        the expected return component type.
+    *           the expected return component type.
     * @return component that matches, or null if none is found.
     */
    @SuppressWarnings("unchecked")
    public <T extends Component> T getComponent(Class<T> type) {
-		return (T)getComponent(ComponentType.getTypeFor(type));
-	}
+      return (T) getComponent(ComponentType.getTypeFor(type));
+   }
 
    /**
-    * Returns a bag of all components this entity has.
-    * You need to reset the bag yourself if you intend to fill it more than once.
+    * Returns a bag of all components this entity has. You need to reset the bag
+    * yourself if you intend to fill it more than once.
     * 
-    * @param fillBag the bag to put the components into.
+    * @param fillBag
+    *           the bag to put the components into.
     * @return the fillBag with the components in.
     */
    public Bag<Component> getComponents(Bag<Component> fillBag) {
@@ -228,29 +231,19 @@ public final class Entity {
    }
 
    /**
-    * (Re)enable the entity in the world, after it having being disabled.
-    * Won't do anything unless it was already disabled.
+    * (Re)enable the entity in the world, after it having being disabled. Won't
+    * do anything unless it was already disabled.
     */
    public void enable() {
       world.enable(this);
    }
 
    /**
-    * Disable the entity from being processed. Won't delete it, it will
-    * continue to exist but won't get processed.
+    * Disable the entity from being processed. Won't delete it, it will continue
+    * to exist but won't get processed.
     */
    public void disable() {
       world.disable(this);
-   }
-
-   /**
-    * Get the UUID for this entity.
-    * This UUID is unique per entity (re-used entities get a new UUID).
-    * 
-    * @return uuid instance for this entity.
-    */
-   public UUID getUuid() {
-      return uuid;
    }
 
    /**
